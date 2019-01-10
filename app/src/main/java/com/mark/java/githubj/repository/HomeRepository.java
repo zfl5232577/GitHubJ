3df3e8989141be100a3d8503523acb8499c612dc
makun.cai <makun.cai@aorise.org>
2019-01-10 10:12:24 +0800

    过滤网络请求数据，否则Type不存在报错

--- app/src/main/java/com/mark/java/githubj/repository/HomeRepository.java
+++ app/src/main/java/com/mark/java/githubj/repository/HomeRepository.java
@@ -12,9 +12,13 @@ import com.mark.java.githubj.module.network.GitHubAPICallback;
 import com.mark.java.githubj.module.network.GitHubAPIObserver;
 import com.mark.java.githubj.module.network.GitHubService;
 
+import java.util.Iterator;
 import java.util.List;
 
 import cn.aorise.common.core.module.network.RetrofitFactory;
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 
 /**
@@ -31,6 +35,20 @@ public class HomeRepository {
     public void queryReceivedEvents(int pageIndex,IBaseListener<List<ReceivedEvent>> listener){
         RetrofitFactory.getInstance().create(GitHubService.class, API.BASE_URL,BuildConfig.DEBUG)
                 .queryReceivedEvents(UserManasger.getInstance().getAuthorization(),UserManasger.getInstance().getUserName(),pageIndex, Constant.PAGE_SIZE)
+                .flatMap(receivedEvents -> {
+                    Iterator<ReceivedEvent> iterator = receivedEvents.iterator();
+                    while (iterator.hasNext()){
+                        ReceivedEvent event = iterator.next();
+                        ReceivedEvent.Type type = event.getType();
+                        if (type!= ReceivedEvent.Type.WatchEvent
+                                &&type!= ReceivedEvent.Type.CreateEvent
+                                &&type!= ReceivedEvent.Type.ForkEvent
+                                &&type!= ReceivedEvent.Type.PushEvent){
+                            iterator.remove();
+                        }
+                    }
+                    return Observable.just(receivedEvents);
+                })
                 .subscribeOn(Schedulers.io())
                 .subscribe(new GitHubAPIObserver<>(null, new GitHubAPICallback<List<ReceivedEvent>>() {
                     @Override
